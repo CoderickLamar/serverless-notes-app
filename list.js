cd1a21ee413b1d8a2fe3f1282eeeee176328aefa
Coderick Lamar <codericklamar@sal3000.local>
2020-05-17 14:34:52 -0700

    finished CRUD design and added basic billing

--- list.js
+++ list.js
@@ -4,20 +4,20 @@ import dynamoDb from "./libs/dynamodb-lib";
 export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
-    Key: {
-        userId: event.requestContext.identity.cognitoIdentityId,
-        noteId: event.pathParameters.id
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId
     }
-
   };
 
-  const result = await dynamoDb.get(params);
-  if (!result.Item) {
-      throw new Error("Item not found.");
-  };
+  const result = await dynamoDb.query(params);
 
-  return result.Item;
+  // Return the matching list of items in response body
+  return result.Items;
 });
